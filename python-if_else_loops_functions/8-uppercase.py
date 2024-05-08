5d456358e7a60522c97b667e48b9293b12b96a31
Thaigerhoo <jhhonathhan42@gmail.com>
2024-05-08 19:38:32 -0400

    Its upper

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uppercase(str):
+    result = ""
+    for char in str:
+        if ord('a') <= ord(char) <= ord('z'):
+            upper_char = chr(ord(char) - 32)
+            result += upper_char
+        else:
+            result += char
+
+        print(result)
