67b3b78be0b7e6227c3071e0ee5907e2a5451492
Thaigerhoo <jhhonathhan42@gmail.com>
2024-05-08 23:23:32 -0400

    Advanced task 1

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for i in range(ord('z'), ord('a') - 1, -1):
-    print(f"{chr(i)}{chr(i - 32) if i % 2 == 0 else ''}", end='')
+print("{}".format(''.join(chr(i) if i % 2 != 0 else chr(i - 32)
+                          for i in range(122, 96, -1))))
