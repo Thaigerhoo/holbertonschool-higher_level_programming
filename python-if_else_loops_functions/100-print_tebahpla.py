fb76bf4c189b1e399aceae573ab95423ddae1495
Thaigerhoo <jhhonathhan42@gmail.com>
2024-05-08 23:59:03 -0400

    Attempted fix

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 count = 0
-output = ""
 for i in range(122, 96, -1):
     if count % 2 == 0:
         output += chr(i)
     else:
         output += chr(i - 32)
     count += 1
-    if count == 25:  # Stop when 26 characters are printed
+    if count == 25:
         break
 
 print("{}".format(output))
