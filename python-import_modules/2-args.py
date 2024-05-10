04e55db85e130da6f9c6bfc301baef6cccd521aa
Thaigerhoo <jhhonathhan42@gmail.com>
2024-05-10 19:07:21 -0400

    Dynamic script

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,8 +4,6 @@ import sys
 
 def print_arguments():
     num_args = len(sys.argv) - 1
-    print("Number of argument" + ("s" if num_args != 1 else "") + ":", end=" ")
-    print(num_args, end=" ")
     print("argument" + ("s" if num_args != 1 else "") + ":", end="")
 
     if num_args == 0:
