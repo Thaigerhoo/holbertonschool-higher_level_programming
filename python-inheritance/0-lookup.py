59acccd3639623ea589531b81a33540832742715
Thaigerhoo <jhhonathhan42@gmail.com>
2024-05-22 17:34:14 -0400

    lookup

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -7,9 +7,14 @@
 
 
 def lookup(obj):
-    """
+    """ Function returns a list of all available
+        attributes and methods of an object.
 
-    Function returns a list of all available attributes and methods of an object
+    Args:
+        obj: instance of the class
+
+    Returns:
+        List of attributes
 
    """
 
