e4c7fd005b3b2b3c2f7e87592a5af9dade4ee607
Thaigerhoo <jhhonathhan42@gmail.com>
2024-05-28 19:55:23 -0400

    Write to a file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3
-"""
-Module containing function that returns the
-number of lines of a text file.
+""" 
+
+Module that contains a function that writes to a text file
 
 """
 
 
-def number_of_lines(filename=""):
-    """ Function that reads from a file and prints its number of lines
+def write_file(filename="", text=""):
+    """ Function that writes to a text file
 
     Args:
-        Filenameis the file's name.
-
+        filename: is the file's name.
+        text: text to write
     Raises
-        Exception: when the file can be opened.
+        Exception: when the file can be opened
 
     """
-    num_lines = 0
-    with open(filename, 'r', encoding="utf-8") as file:
-        for line in file:
-            num_lines += 1
-    return num_lines
+
+    with open(filename, 'w', encoding="utf-8") as f:
+        return f.write(text)
