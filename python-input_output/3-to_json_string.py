be782713a0c9b28996b88b249d0a083742495005
Thaigerhoo <jhhonathhan42@gmail.com>
2024-05-28 20:00:54 -0400

    To JSON string

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+
+Module that contains a function that returns the JSON
+representation of an object.
+
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """ Function that returns the JSON representation of an object
+
+    Args:
+        my_obj: Is an object
+
+    Raises:
+        Exception: when the object can't be encoded
+
+    """
+    return json.dumps(my_obj)
