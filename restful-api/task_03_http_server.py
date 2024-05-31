ad8e53d8f6cf5b495bf035701f44710d0dd84c41
Thaigerhoo <jhhonathhan42@gmail.com>
2024-05-31 13:30:18 -0400

    Fixed wrong def

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,9 +1,8 @@
 import http.server
-import socketserver
 import json
 
 
-class MyHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
+class MyHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == '/':
             self.send_response(200)
@@ -11,10 +10,10 @@ class MyHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(b"Hello, this is a simple API!")
         elif self.path == '/data':
+            data = {"name": "John", "age": 30, "city": "New York"}
             self.send_response(200)
             self.send_header("Content-type", "application/json")
             self.end_headers()
-            data = {"name": "John", "age": 30, "city": "New York"}
             self.wfile.write(json.dumps(data).encode())
         elif self.path == '/status':
             self.send_response(200)
@@ -23,6 +22,12 @@ class MyHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             self.wfile.write(b"OK")
         else:
             self.send_response(404)
-            self.send_header("Content-type", "text/plain")
+            self.send_header("Content-type", "application/json")
             self.end_headers()
-            self.wfile.write(b"Endpoint not found")
+            self.wfile.write(b"404 not found")
+
+
+if __name__ == "__main__":
+    server_address = ('', 8000)
+    httpd = http.server.HTTPServer(server_address, MyHandler)
+    httpd.serve_forever()
