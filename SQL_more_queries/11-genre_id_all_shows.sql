4c8bfcb80b62ec42bae82eebc3bd01e39239b0aa
Thaigerhoo <jhhonathhan42@gmail.com>
2024-06-18 21:55:20 -0400

    Syntax error

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,5 @@
 -- Script that list all shows contained in a databse.
 SELECT DISTINCT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.shows_id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_shows_genres.genre_id;
