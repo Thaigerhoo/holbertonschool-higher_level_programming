b8e4b35ad6b028d8eab378a2e781ae517c8d3a7e
Thaigerhoo <jhhonathhan42@gmail.com>
2024-06-20 22:56:14 -0400

    Working on advanced tasks

--- python-inheritance/100-my_int.py
+++ python-inheritance/100-my_int.py
@@ -5,11 +5,12 @@ Module containing a rebel class.
 
 """
 
+
 class MyInt(int):
     def __eq__(self, other):
         """ Method that overrides the == operator to act like != """
         return int(self) != other
 
     def __ne__(self, other):
-         """ Method that overrides the != operator to act like == """
-         return int(self) == other
+        """ Method that overrides the != operator to act like == """
+        return int(self) == other
