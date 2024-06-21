af2eb814ef3cb21f0c19e361575191e5e8cb1807
Thaigerhoo <jhhonathhan42@gmail.com>
2024-06-21 18:00:29 -0400

    Added first two tasks

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+
+Module containing a script that list all states with added specifications
+
+"""
+
+
+if __name__ == '__main__':
+    import MySQLdb
+    from sys import argv
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM states WHERE
+                name LIKE BINARY "N%" ORDER BY id""")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
