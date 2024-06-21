0260cadb4417cb975dcda284f699a8de10141cfa
Thaigerhoo <jhhonathhan42@gmail.com>
2024-06-21 18:04:48 -0400

    Get all states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""
+
+Module containing a script that lists all states.
+
+"""
 import sys
 import MySQLdb
+
 
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
