36dde86ff1137e27dfad44629fc30a286fa73c86
Thaigerhoo <jhhonathhan42@gmail.com>
2024-06-23 20:03:27 -0400

    Search and update

--- python-input_output/100-append_after.py
+++ python-input_output/100-append_after.py
@@ -23,7 +23,7 @@ def append_after(filename="", search_string="", new_string=""):
             modified_lines.append(line)
 
             if search_string in line:
-                modified_lines.append(new_string + '\n')
+                modified_lines.append(new_string)
 
     with open(filename, 'w') as file:
         file.writelines(modified_lines)
